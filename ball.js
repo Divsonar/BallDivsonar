5f09dc501d0dd22d77f435360ff39007eb83906d
Divsonar <83642112+Divsonar@users.noreply.github.com>
2022-11-10 21:51:44 +0900

    V1.1

--- ball.js
+++ ball.js
@@ -3,16 +3,17 @@ import { AsciiEffect } from './packages/asciieffect.js';
 import { TrackballControls } from './packages/trackballcontrols.js';
 import { GLTFLoader } from './packages/gltfloader.js';
 
-let camera, controls, scene, renderer, effect;
+let camera, scene, renderer;
 
-let sphere, plane, cube;
+let sphere, plane, cube, phone, phoneload;
+var modelnumber;
 
 const start = Date.now();
 
 function init() {
 
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.y = -50;
+    camera.position.y = -20;
     camera.position.z = 0;
 
     scene = new THREE.Scene();
@@ -20,28 +21,41 @@ function init() {
     scene.background = skyTexture;
     // scene.background = new THREE.Color(0,0,0);
 
-    const pointLight1 = new THREE.PointLight(0xffffff);
-    pointLight1.position.set(0, 200, 0);
-    scene.add(pointLight1);
+    // const pointLight1 = new THREE.PointLight(0xffffff);
+    // pointLight1.position.set(0, 1000, 500);
+    // scene.add(pointLight1);
 
-    const pointLight2 = new THREE.PointLight(0xffffff, 0.25);
-    pointLight2.position.set(500, 500, 500);
+    const pointLight2 = new THREE.PointLight(0xffffff, 1.5);
+    pointLight2.position.set(400, -150, 200);
     scene.add(pointLight2);
 
-    const pointLight3 = new THREE.PointLight(0xffffff, 0.25);
-    pointLight3.position.set(- 500, - 500, - 500);
+    const pointLight3 = new THREE.PointLight(0xffffff, 1);
+    pointLight3.position.set(-400, 150, -200);
     scene.add(pointLight3);
 
     sphere = new THREE.Mesh(new THREE.SphereGeometry(200, 20, 10), new THREE.MeshPhongMaterial({ flatShading: true }));
 
-    const laptoploader = new GLTFLoader();
-    laptoploader.load("./textures/dibesfer_voxel_pc/scene.gltf", ( gltf ) => {
-            gltf.scene.position.z = 150
-            gltf.scene.position.x = -100
-            gltf.scene.position.y = -200;
-            gltf.scene.rotation.y = -0.75
+    const deskloader = new GLTFLoader();
+    deskloader.load("textures/simple_dirty_desk/scene.gltf", ( gltf ) => {
+            gltf.scene.position.z = 255
+            gltf.scene.position.x = -65
+            gltf.scene.position.y = -205;
+            gltf.scene.rotation.y = 0.61
             // gltf.scene.rotation.y = -1.5;
-            gltf.scene.scale.set(50,50,50);
+            gltf.scene.scale.set(175,175,175);
+            scene.add(gltf.scene) ;
+            render();
+        },
+    );
+
+    const desktoploader = new GLTFLoader();
+    desktoploader.load("./textures/gaming_desktop_pc/scene.gltf", ( gltf ) => {
+            gltf.scene.position.z = 200
+            gltf.scene.position.x = -0
+            gltf.scene.position.y = -70;
+            gltf.scene.rotation.y = -0.95
+            // gltf.scene.rotation.y = -1.5;
+            gltf.scene.scale.set(25,25,25);
             scene.add(gltf.scene) ;
             render();
         },
@@ -49,27 +63,29 @@ function init() {
 
     const pianoloader = new GLTFLoader();
     pianoloader.load("./textures/grand_piano_1mb/scene.gltf", ( gltf ) => {
-            gltf.scene.position.z = -150; 
-            gltf.scene.position.x = -150; 
+            gltf.scene.position.z = -400; 
+            gltf.scene.position.x = -135; 
             gltf.scene.position.y = -200;
             gltf.scene.rotation.y = 0;
-            gltf.scene.scale.set(7,7,7);
+            gltf.scene.scale.set(6.25,6.25,6.25);
             scene.add(gltf.scene) ;
             render();
         },
     );
 
-    // const animeloader = new GLTFLoader();
-    // animeloader.load("./textures/blanket_in_the_wind/scene.gltf", ( gltf ) => {
-    //         gltf.scene.position.z = 125; 
-    //         gltf.scene.position.x = 100
-    //         gltf.scene.position.y = -150;
-    //         gltf.scene.rotation.y = -0.75;
-    //         gltf.scene.scale.set(0.75,0.75,0.75);
-    //         scene.add(gltf.scene) ;
-    //         render();
-    //     },
-    // );
+    phone = new GLTFLoader();
+    phone.load("textures/samsung_galaxy_s21_ultra/scene.gltf", ( gltf ) => {
+            console.log(gltf.scene.id)
+            modelnumber = gltf.scene.id 
+            gltf.scene.position.z = 125; 
+            // gltf.scene.position.x = 150
+            // gltf.scene.position.y = -200;
+            // gltf.scene.rotation.y = -0.75;
+            gltf.scene.scale.set(0.7,0.7,0.7);
+            scene.add(gltf.scene) ;
+            render();
+        },
+    );
 
     const cubeTexture = new THREE.TextureLoader().load('pepega.png')
     cubeTexture.wrapS = THREE.RepeatWrapping;
@@ -81,10 +97,10 @@ function init() {
 
     // Plane
 
-    const planeTexture = new THREE.TextureLoader().load('./textures/floor2.jpg')
+    const planeTexture = new THREE.TextureLoader().load('./textures/marble.jpg')
     planeTexture.wrapS = THREE.RepeatWrapping;
     planeTexture.wrapT = THREE.RepeatWrapping;
-    planeTexture.repeat.set( 2, 20 );
+    planeTexture.repeat.set( 4, 40 );
     plane = new THREE.Mesh(new THREE.PlaneGeometry(500, 10000), new THREE.MeshBasicMaterial({ color: 0xe0e0e0, wireframe: false, map:planeTexture}));
     plane.position.y = - 200;
     plane.rotation.x = - Math.PI / 2;
@@ -146,18 +162,35 @@ function moveCamera() {
 //     camera.position.z = 500;
 // }
 
-function render() {
+async function render() {
 
     const timer = Date.now() - start;
+    for (var i = 0; i < scene.children.length; i++)
+        if (scene.children[i].id == modelnumber) {
+            scene.children[i].position.set(100, Math.sin(timer * 0.001) * 10 - 15, 190);
+            scene.children[i].rotation.set(0, timer * 0.0010 ,0);
+        } 
+    // console.log(scene)
+    // await scene.children[modelnumber].position.set(125, Math.sin(timer * 0.001) * 20, 150);
+    // await scene.children[modelnumber].rotation.set(0, timer * 0.0010 ,0);
+    // gltf.scene.position.y = ;
+    // gltf.scene.position.x = ;
+    // gltf.scene.position.z = ;
 
-    // sphere.position.y = Math.abs(Math.sin(timer * 0.002)) * 150;
-    // sphere.rotation.x = timer * 0.0003;
-    // sphere.rotation.z = timer * 0.0002;
-
-    cube.position.y = Math.abs(Math.sin(timer * 0.002)) * 150;
-    cube.rotation.x = timer * 0.0003;
-    cube.rotation.z = timer * 0.0002;
+    // cube.position.y = Math.abs(Math.sin(timer * 0.002)) * 150;
+    // cube.rotation.x = timer * 0.0003;
+    // cube.rotation.z = timer * 0.0002;
     
+    const targetAspect = window.innerWidth / window.innerHeight;
+    const imageAspect = 3840 / 2160;
+    const factor = imageAspect / targetAspect;
+    // When factor larger than 1, that means texture 'wilder' than target。 
+    // we should scale texture height to target height and then 'map' the center  of texture to target， and vice versa.
+    scene.background.offset.x = factor > 1 ? (1 - 1 / factor) / 2 : 0;
+    scene.background.repeat.x = factor > 1 ? 1 / factor : 1;
+    scene.background.offset.y = factor > 1 ? 0 : (1 - factor) / 2;
+    scene.background.repeat.y = factor > 1 ? 1 : factor;
+
     // controls.update();
 
     renderer.render(scene, camera);
