5f03d03b87098c27a8388f30e4a139100cb9897a
Divsonar <83642112+Divsonar@users.noreply.github.com>
2022-11-26 14:24:56 +0900

    V1.5
    Added Sky, Grass, Door, OutsideLight
    Fixed Loading Screen for Desktop and Mobile
    Modified moveCamera to move in ratio to mobile resolution

--- ball.js
+++ ball.js
@@ -33,7 +33,7 @@ function init() {
     // Scene
 
     scene = new THREE.Scene();
-    const skyTexture = new THREE.TextureLoader().load('./textures/bg.jpg');
+    const skyTexture = new THREE.TextureLoader().load('./textures/bg2.jpg');
     scene.background = skyTexture;
     // scene.background = new THREE.Color(0,0,0);
 
@@ -98,8 +98,24 @@ function init() {
      scene.add( lamp2Light );
 
 
+     //  const signGeometry = new THREE.SphereGeometry( 5, 16, 8 );
+     var outsideLight = new THREE.PointLight( 0xFFFFFF, 1000000, 1000, 2 );
+    //  var signMat = new THREE.MeshStandardMaterial( {
+    //      emissive: 0xF000FF,
+    //      emissiveIntensity: 1,
+    //      color: 0xF000FF
+    //  } );
+    //  lamp2Light.add( new THREE.Mesh( signGeometry, signMat ) );
+    outsideLight.shadow.bias = 0.02;
+    outsideLight.position.set( 100, 100, -1700 );
+    outsideLight.castShadow = true;
+    outsideLight.shadow.radius = 5
+    outsideLight.shadow.blurSamples = 50
+    scene.add(outsideLight);
 
-    var ambientLight = new THREE.AmbientLight( 0xffee88, 0.05, 1000, 2 );
+
+
+    var ambientLight = new THREE.AmbientLight( 0xffee88, 0.1, 1000, 2 );
     ambientLight.position.set( 0, -20, -200 );
     // ambientLight.castShadow = true; 0.15
     scene.add( ambientLight );
@@ -372,7 +388,25 @@ function init() {
         },
     );
 
-
+    const doorloader = new GLTFLoader(loadingManager);
+    doorloader.load("textures/opened_patio_door/scene.gltf", ( gltf ) => {
+            gltf.scene.position.z = -1350; 
+            gltf.scene.position.x = -100; 
+            gltf.scene.position.y = -90;
+            gltf.scene.rotation.y = 0;
+            gltf.scene.scale.set(20,20,20);
+            gltf.scene.traverse(function (child) {
+                if (child.isMesh) {
+                  child.castShadow = true;
+                  child.receiveShadow = true;
+                }
+             });
+            gltf.scene.castShadow = true;
+            gltf.scene.receiveShadow = true;
+            scene.add(gltf.scene) ;
+            render();
+        },
+    );
 
 
 
@@ -383,7 +417,7 @@ function init() {
     bricktexture.wrapT = THREE.RepeatWrapping;
     bricktexture.repeat.set( 6, 8 );
     
-    const leftwall = new THREE.Mesh( new THREE.BoxGeometry( 4000, 2000, 0.1 ), new THREE.MeshStandardMaterial( { flatShading: false, map:bricktexture} ) );
+    const leftwall = new THREE.Mesh( new THREE.BoxGeometry( 2000, 2000, 0.1 ), new THREE.MeshStandardMaterial( { flatShading: false, map:bricktexture} ) );
     leftwall.rotation.y = Math.PI /2
     leftwall.position.y = -150
     leftwall.position.x = -255
@@ -392,7 +426,7 @@ function init() {
     leftwall.receiveShadow = true;
     scene.add( leftwall );
 
-    const rightwall = new THREE.Mesh( new THREE.BoxGeometry( 4000, 2000, 0.1 ), new THREE.MeshStandardMaterial( { flatShading: false, map:bricktexture} ) );
+    const rightwall = new THREE.Mesh( new THREE.BoxGeometry( 2000, 2000, 0.1 ), new THREE.MeshStandardMaterial( { flatShading: false, map:bricktexture} ) );
     rightwall.rotation.y = Math.PI /2
     rightwall.position.y = -150
     rightwall.position.x = 255
@@ -401,7 +435,7 @@ function init() {
     rightwall.receiveShadow = true;
     scene.add( rightwall );
 
-    const topwall = new THREE.Mesh( new THREE.BoxGeometry( 2000, 4000, 1 ), new THREE.MeshStandardMaterial( { flatShading: false, map:bricktexture} ) );
+    const topwall = new THREE.Mesh( new THREE.BoxGeometry( 2000, 2000, 1 ), new THREE.MeshStandardMaterial( { flatShading: false, map:bricktexture} ) );
     topwall.rotation.x = Math.PI/2
     topwall.position.y = 350
     topwall.position.x = 100
@@ -410,19 +444,65 @@ function init() {
     topwall.receiveShadow = true;
     scene.add( topwall );
 
+    const frontbricktexture = new THREE.TextureLoader().load('textures/brickwall.jpg')
+    frontbricktexture.wrapS = THREE.RepeatWrapping;
+    frontbricktexture.wrapT = THREE.RepeatWrapping;
+    frontbricktexture.repeat.set( 1, 1 );
+
+    const frontwall = new THREE.Mesh( new THREE.BoxGeometry( 530, 300, 0.3 ), new THREE.MeshStandardMaterial( { flatShading: false, map:frontbricktexture} ) );
+    frontwall.rotation.x = 0
+    frontwall.position.y = 240
+    frontwall.position.x = 0
+    frontwall.position.z = -1350
+    frontwall.castShadow = true;
+    frontwall.receiveShadow = true;
+    scene.add( frontwall );
+
+    const frontwall2 = new THREE.Mesh( new THREE.BoxGeometry( 300, 300, 0.3 ), new THREE.MeshStandardMaterial( { flatShading: false, map:frontbricktexture} ) );
+    frontwall2.rotation.x = 0
+    frontwall2.position.y = -55
+    frontwall2.position.x = 200
+    frontwall2.position.z = -1350
+    frontwall2.castShadow = true;
+    frontwall2.receiveShadow = true;
+    scene.add( frontwall2 );
+
+    const frontwall3 = new THREE.Mesh( new THREE.BoxGeometry( 300, 300, 0.3 ), new THREE.MeshStandardMaterial( { flatShading: false, map:frontbricktexture} ) );
+    frontwall3.rotation.x = 0
+    frontwall3.position.y = -55
+    frontwall3.position.x = -340
+    frontwall3.position.z = -1350
+    frontwall3.castShadow = true;
+    frontwall3.receiveShadow = true;
+    scene.add( frontwall3 );
+
+
+
+
     // Plane
 
     const planeTexture = new THREE.TextureLoader().load('./textures/marble.jpg')
     planeTexture.wrapS = THREE.RepeatWrapping;
     planeTexture.wrapT = THREE.RepeatWrapping;
     planeTexture.repeat.set( 4, 40 );
-    plane = new THREE.Mesh(new THREE.PlaneGeometry(500, 10000), new THREE.MeshPhongMaterial({ color: 0xe0e0e0, wireframe: false, map:planeTexture}));
+    plane = new THREE.Mesh(new THREE.PlaneGeometry(500, 2700), new THREE.MeshPhongMaterial({ color: 0xe0e0e0, wireframe: false, map:planeTexture}));
     plane.position.y = - 200;
     plane.rotation.x = - Math.PI / 2;
     plane.castShadow = true;
     plane.receiveShadow = true;
     scene.add(plane);
 
+    const grassTexture = new THREE.TextureLoader().load('./textures/grass.jpg')
+    grassTexture.wrapS = THREE.RepeatWrapping;
+    grassTexture.wrapT = THREE.RepeatWrapping;
+    grassTexture.repeat.set( 30, 40 );
+    const grass = new THREE.Mesh(new THREE.PlaneGeometry(4000, 2000), new THREE.MeshPhongMaterial({ color: 0xe0e0e0, wireframe: false, map:grassTexture}));
+    grass.position.y = - 200;
+    grass.rotation.x = - Math.PI / 2;
+    grass.position.z = - 2345;
+    grass.castShadow = true;
+    grass.receiveShadow = true;
+    scene.add(grass);
 
 
     // const textureLoader = new THREE.TextureLoader();
@@ -501,7 +581,15 @@ function animate() {
 
 function moveCamera() {
     const t = document.body.getBoundingClientRect().top + 10000 * 0.2;
-    camera.position.z = t * 0.25
+    console.log(t)
+    console.log(window.innerWidth)
+    var l = (window.innerWidth/1280)
+    if (l<1) {
+        camera.position.z = (t * (0.25*l))+400
+    }
+    else {
+        camera.position.z = (t * (0.25*l))
+    }
     camera.position.x = 0
     console.log("Camera Z Pos:" + camera.position.z)
     // camera.position.x = t * -0.02;
